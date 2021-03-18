1c589d8599e6f0cd9f43fb72e2f4e5774566aea1
Галина <galinamyznikova@list.ru>
2021-03-18 09:54:37 +0300

    myapp_v2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +12,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
